bb124be1b3b9e18b0232ab0e3e25e02538eab6df
warglaivee <warglaivegm@gmail.com>
2017-05-02 00:58:02 +0300

    PC

---
+++ 01. Count Real Numbers/01. Count Real Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Count_Real_Numbers
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+            var counts = new SortedDictionary<double, int>();
+            foreach (var num in input)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (var num in counts)
+            {
+                Console.WriteLine("{0} -> {1}",num.Key, num.Value);
+            }
+        }
+    }
+}
---
+++ 02. Odd Occurrences/02. Odd Occurrences.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Odd_Occurrences
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine().ToLower().Split().ToList();
+            var dict = new Dictionary<string, int>();
+            var result = new List<string>();
+            foreach (var word in input)
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                }
+                else
+                {
+                    dict[word] = 1;
+                }
+            }
+            foreach (var word in dict)
+            {
+                if (word.Value % 2 == 1)
+                {
+                    result.Add(word.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ",result));
+        }
+    }
+}
