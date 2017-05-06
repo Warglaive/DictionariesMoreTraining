c81dc7dd3eabe194e70225518d96a094e2fc3b43
warglaivee <warglaivegm@gmail.com>
2017-05-07 02:19:10 +0300

    MixePhones Written
    ExamShoppint - to do

---
+++ 03. Mixed Phones/03. Mixed Phones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Mixed_Phones
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new Dictionary<string, string>();
+            while (input!="Over")
+            {
+                var inputParts = input.Split(new[] { ' ', ':' }
+                , StringSplitOptions.RemoveEmptyEntries);
+                var firstElement = inputParts[0];
+                var secondElement = inputParts[1];
+                var firstNumber = 0;
+                long secondNumber = 0;
+                if (int.TryParse(firstElement, out firstNumber))
+                {
+                    result[secondElement] = firstNumber.ToString();
+                }
+                else
+                {
+                    secondNumber = long.Parse(secondElement);
+                    result[firstElement] = secondNumber.ToString();
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var KVP in result.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{KVP.Key} -> {KVP.Value}");
+            }
+        }
+    }
+}
