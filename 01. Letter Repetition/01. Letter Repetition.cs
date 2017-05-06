69905728034aac8ba9d287084133ad21924556dd
warglaivee <warglaivegm@gmail.com>
2017-05-07 00:50:22 +0300

    02. Dict-Ref pc written

---
+++ 01. Letter Repetition/01. Letter Repetition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Letter_Repetition
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var entry = Console.ReadLine();
+            var dict = new Dictionary<char, int>();
+            var result = entry.ToCharArray();
+            foreach (var character in result)
+            {
+                if (!dict.ContainsKey(character))
+                {
+                    dict[character] = 1;
+                }
+                else
+                {
+                    dict[character]++;
+                }
+            }
+            foreach (var character in dict)
+            {
+                Console.WriteLine($"{character.Key} -> {character.Value}");
+            }
+        }
+    }
+}
---
+++ 02. Dict-Ref/02. Dict-Ref.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.Dict_Ref
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new Dictionary<string, int>();
+            while (input!="end")
+            {
+                var inputParts = input
+                .Split(new[] { ' ', '=' }
+            , StringSplitOptions.RemoveEmptyEntries);
+                bool isInt = false;
+                var name = inputParts[0];
+                var secondName = string.Empty;
+                var number = 0;
+                isInt = int.TryParse(inputParts[1], out number);
+                if (isInt == false)
+                {
+                    secondName = inputParts[1];
+                    if (result.ContainsKey(secondName))
+                    {
+                        result[name] = result[secondName];
+                    }
+                }
+                if (isInt == true)
+                {
+                    result[name] = number;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var KVP in result)
+            {
+                Console.WriteLine($"{KVP.Key} === {KVP.Value}");
+            }
+        }
+    }
+}
