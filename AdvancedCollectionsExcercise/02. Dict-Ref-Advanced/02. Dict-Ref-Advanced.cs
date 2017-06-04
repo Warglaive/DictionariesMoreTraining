7cdb2041da3ca4f82e693ac1aa6150b28752c20c
warglaivee <warglaivegm@gmail.com>
2017-06-04 21:22:53 +0300

    06.04.2017 9:22 PM
    03. Forum Topics is hardcore shit made by bastard

---
+++ AdvancedCollectionsExcercise/02. Dict-Ref-Advanced/02. Dict-Ref-Advanced.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Dict_Ref_Advanced
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var result = new Dictionary<string, List<string>>();
+            var input = Console.ReadLine();
+            while (input != "end")
+            {
+                var inputParams = input
+                    .Split(new char[] { ' ', '-', '>', ',' }
+                    , StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                var name = inputParams[0];
+                var intValues = inputParams.Skip(1).ToList();
+                var number = -1;
+                var anotherKey = string.Empty;
+
+                foreach (var intValue in intValues)
+                {
+                    var isInt = int.TryParse(intValue, out number); //if value is given
+                    var numToStr = string.Empty;
+
+                    if (isInt)
+                    {
+                        if (!result.ContainsKey(name)) //save all names as keys
+                        {
+                            result[name] = new List<string>();
+                        }
+                        numToStr = number.ToString();
+                        result[name].Add(numToStr);
+                    }
+                    else //if another key is given
+                    {
+                        var secondKey = intValues[0];
+                        if (result.ContainsKey(secondKey)) //if second key dosent exist
+                        {
+                            if (!result.ContainsKey(name)) //save all names as keys
+                            {
+                                result[name] = new List<string>();
+                            }
+                            result[name] = result[secondKey]; // add value to second key
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} === {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
---
+++ AdvancedCollectionsExcercise/03. Forum Topics/03. Forum Topics.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Forum_Topics
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var result = new Dictionary<string, HashSet<string>>();
+            var input = Console.ReadLine();
+            while (input != "filter") 
+            {
+                var inputParts = input
+                    .Split(new[] { '-', '>', ' ', ',' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                var topic = inputParts[0];
+                var tags = inputParts.Skip(1).ToList();
+
+                if (!result.ContainsKey(topic))
+                {
+                    result[topic] = new HashSet<string>();
+                }
+                foreach (var item in tags)
+                {
+                    item.ToString();
+                    result[topic].Add(item);
+                }
+                input = Console.ReadLine();
+            }
+            var tagsCheck = Console.ReadLine()
+                .Split(new[] { ' ', ',' }
+            , StringSplitOptions
+                .RemoveEmptyEntries)
+                .ToList();
+            bool contained = false;
+            foreach (var KVP in result.Keys)
+            {
+                for (int i = 0; i < tagsCheck.Count; i++)
+                {
+                    if (result[KVP].Contains(tagsCheck[i]))
+                    {
+                        contained = true;
+                    }
+                    else
+                    {
+                        contained = false;
+                    }
+                    if (tagsCheck.Count >= 2 && contained==false)
+                    {
+                        i = tagsCheck.Count + 1;
+                    }
+                }
+                if (contained)
+                {
+                    Console.WriteLine($"{KVP} | #{string.Join(", #",result[KVP])}");
+                }
+            }
+        }
+    }
+}
